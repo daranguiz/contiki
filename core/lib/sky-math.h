c1e3c4122d69dc7b4dd54f6dcd61096b86b73e30
Kyle Harris <kaharrs2@gmail.com>
2012-06-13 03:07:10 -0500

    added support for a new library sky-math to core/lib/

---
+++ core/lib/sky-math.h
@@ -0,0 +1,11 @@
+#ifndef __SKY_MATH_H__
+#define __SKY_MATH_H__
+
+// Function declarations
+unsigned short abs_sub(unsigned short var1, unsigned short var2);
+unsigned short mypow2(unsigned short base);
+unsigned short log_exp_term(unsigned short var1, unsigned short var2);
+int mylog(unsigned short var);
+unsigned short mysqrt(unsigned short var);
+
+#endif /* __SKY_MATH_H__ */
